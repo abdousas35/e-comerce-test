760be38f7cc7f19a04309803a4eceb1706d715b5
abdousas35 <abdousas35@gmail.com>
2026-07-22 08:50:42 +0100

    bug-mail4

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -5,10 +5,12 @@ export const sendEmail = async (options) => {
         throw new Error("Brevo API key is not configured");
     }
 
+    console.log("SENDER EMAIL USED:", process.env.SMTP_MAIL);
+
     const client = new BrevoClient({ apiKey: process.env.BREVO_API_KEY });
 
     await client.transactionalEmails.sendTransacEmail({
-        sender: { email: process.env.SMTP_MAIL, name: "Your Store Name" },
+        sender: { email: process.env.SMTP_MAIL, name: "Shop Easy" },
         to: [{ email: options.email }],
         subject: options.subject,
         textContent: options.message,
