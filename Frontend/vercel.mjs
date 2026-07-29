0db519ec9f49159d54d7857b29aff004ee41574b
abdousas35 <abdousas35@gmail.com>
2026-07-29 20:56:34 +0100

    besto68

---
+++ Frontend/vercel.mjs
@@ -0,0 +1,23 @@
+import { routes } from '@vercel/config/v1';
+
+export const config = {
+  rewrites: [
+    routes.rewrite(
+      '/(.*)',
+      'https://service.prerender.io/https://www.easy-shopping-official.com/$1',
+      {
+        has: [
+          {
+            type: 'header',
+            key: 'user-agent',
+            value: '/.*(googlebot|bingbot|yandex|baiduspider|facebookexternalhit|twitterbot|rogerbot|linkedinbot|embedly|quora link preview|showyouhaveit|outbrain|pinterest|slackbot|vkshare|w3c_validator|whatsapp).*/i',
+          },
+        ],
+        requestHeaders: {
+          'X-Prerender-Token': process.env.PRERENDER_TOKEN,
+        },
+      },
+    ),
+    routes.rewrite('/(.*)', '/index.html'),
+  ],
+};
